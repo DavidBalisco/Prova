41a507c597b7e19cb919b8b124a4de264ad47778
DavidBalisco <davidefran4@gmail.com>
2024-03-11 16:22:06 +0100

    inter

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main{
 	public static void main(String args[]){
 		System.out.println(args[0]);
-		
-		
+		int inter= 0;
+		if(inter==0){
+			System.out.println("Forza Inter");
+		}
 		
 	}
 }
