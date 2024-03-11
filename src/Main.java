e83f282ca35afe2cd08a7fea0ba1d6d2283ea5d9
DavidBalisco <davidefran4@gmail.com>
2024-03-11 16:51:51 +0100

    fix

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main{
 	public static void main(String args[]){
-		System.out.println(args[0]);
-		int inter= 0;
-		if(inter==0){
-			System.out.println("Forza Inter");
-			System.out.println("Juve seconda");
+		int i=0;
+		if(i==0){
+			System.out.println("i è uguale a 0");
 		}
 		
 	}
